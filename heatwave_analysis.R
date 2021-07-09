41e2f37de34e6193a29c4149ed1f9269c542bb95
kimberly-bastille <kimberly.bastille@gmail.com>
2021-07-09 09:09:34 -0400

    add heatwave analysis flame plot

--- heatwave_analysis.R
+++ heatwave_analysis.R
@@ -24,12 +24,34 @@ nar2<-read_csv(file.path(raw.dir,narts_heatwave_csv),
   # Narragansett
 ts <- heatwaveR::ts2clm(nar2, climatologyPeriod = c("2004-01-01", "2019-12-31"))
 nar.mhw <- heatwaveR::detect_event(ts)
-nar.hw<- nar.mhw$event %>%
-    dplyr::select(event_no, duration, date_start, date_peak, intensity_max, intensity_cumulative)%>%
-    dplyr::mutate(Site = "NAR")
+  
+mhw<- nar.mhw$clim %>%
+  mutate(Site = c("NAR"),
+         Year = c("2020"))
+mhw.nar.year <- mhw[5845:6210,] ## Pull out just 2020
 
 heatwaveR::lolli_plot(nar.mhw)
-  
+
+mhw.nar.year %>% 
+  filter(Site == "NAR", 
+         str_detect(t, "2020")) %>% 
+  ggplot( aes(x = t, y = temp))+
+  geom_flame(aes(y2 = thresh))+ 
+  geom_line(aes(x = t, y = seas, color = "a"), size = 1)+
+  geom_line(aes(x = t, y = thresh, color = "c"), size = 1)+
+  geom_line(aes(x = t, y = temp, color = "b"))+
+  scale_colour_manual(values = c("turquoise4", "sienna3", "black"),
+                      labels = c("Climatology","Temperature", "Threshold"))+
+  ylab("Temperature (C)")+
+  xlab(element_blank())+
+  scale_x_date(date_labels = "%b", breaks = "1 month")+
+  theme_bw()+
+  theme(legend.title = element_blank(),
+        legend.position=c(0.2, 0.8))+
+  ecodata::theme_title()
+
+
+
   # #GOM
   # ts <- heatwaveR::ts2clm(gom, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   # gom.mhw <- heatwaveR::detect_event(ts)
@@ -80,7 +102,7 @@ heatwaveR::lolli_plot(nar.mhw)
 #   mab<-read_csv(file.path(raw.dir,"MAB_OISST.csv"),
 #                 col_types = cols(temp = col_double(),t = col_date()))
 #   #GB
-#   ts <- heatwaveR::ts2clm(gb, climatologyPeriod = c("1982-01-01", "2011-12-31"))
+#  ts <- heatwaveR::ts2clm(gb, climatologyPeriod = c("1982-01-01", "2011-12-31"))
 #   gb.mhw <- heatwaveR::detect_event(ts)
 #   #GOM
 #   ts <- heatwaveR::ts2clm(gom, climatologyPeriod = c("1982-01-01", "2011-12-31"))
@@ -91,7 +113,7 @@ heatwaveR::lolli_plot(nar.mhw)
 #   
 #   ### Take just clim
 #   #GB
-#   mhw<- gb.mhw$clim %>%
+#  mhw<- gb.mhw$clim %>%
 #     mutate(EPU = c("GB"),
 #            Year = c("2020"))# add EPU column
 #   mhw.gb.year <- mhw[13880:14223,]## days in 2020 data set only went to dec 9, 2020
